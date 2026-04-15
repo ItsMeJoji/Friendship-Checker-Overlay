fc93f01d95d59400de5afa99aaf133dad37d7de3
ItsMeJoji <itsmejojitcg@gmail.com>
2026-04-15 15:55:30 -0700

    0.3.7 - Updating Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vite';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   // Set the base path to match the GitHub repository name
@@ -6,6 +10,12 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        help: resolve(__dirname, 'help/index.html'),
+      },
+    },
   },
   server: {
     port: 3000,
